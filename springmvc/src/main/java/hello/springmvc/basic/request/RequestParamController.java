8c27be54283501719b0245cbf6b2c947de8a22b2
kimseokjin <kimseokjin0324@gmail.com>
2021-09-05 02:21:05 +0900

    HTTP 요청 파라미터- @RequestParam 2021/09/05

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -3,10 +3,13 @@ package hello.springmvc.basic.request;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -20,4 +23,63 @@ public class RequestParamController {
 
         response.getWriter().write("ok");
     }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+        @RequestParam("username") String memberName,
+        @RequestParam("age") int memberAge){
+
+        log.info("username={}, age={}",memberName,memberAge);
+        return "ok"; //원래 컨트롤러에서 String이면 뷰를 반환해서 @ResponseBody를 애노테이션으로 지정해주면된다.
+    }
+
+
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age){
+
+        log.info("username={}, age={}",username,age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username,int age){
+        log.info("username={}, age={}",username,age);
+        return "ok";
+    }
+    //v3보다 더 편리하게 @RequestParam을 지울수있다 조건) 요청 파라미터와 같아야함
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username, //required= true면 꼭 username에 값이 들어와야함 없으면 팅김
+            @RequestParam(required = false) Integer age ){
+        //int는 null이 들어갈수 없다. Integer는 자바에서 객체이므로 null 값이 들어갈수 있다.
+        log.info("username={}, age={}",username,age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(
+            @RequestParam(required = true, defaultValue = "guest") String username,
+            @RequestParam(required = false, defaultValue = "-1") int age ){
+
+        log.info("username={}, age={}",username,age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String , Object> paramMap){
+
+        log.info("username={}, age={}",paramMap.get("username"),paramMap.get("age"));
+        return "ok";
+    }
+
+
+
 }
